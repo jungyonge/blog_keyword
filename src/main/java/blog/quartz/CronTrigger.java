f04fc31f9ae84f4a392b258cdaf4408d9c74b662
jungyonge <wjddyd.1204>
2020-03-02 11:05:59 +0900

    쿠팡 파트너스 네이버블로그 크론 설정 수정

--- src/main/java/blog/quartz/CronTrigger.java
+++ src/main/java/blog/quartz/CronTrigger.java
@@ -15,15 +15,15 @@ public class CronTrigger {
         scheduler.start();
 
         //job 지정
-        JobDetail job = JobBuilder.newJob(SendEmail.class).withIdentity("SendEmail").build();
-        JobDetail job1 = JobBuilder.newJob(SendEmail.class).withIdentity("BlogPost").build();
+//        JobDetail job = JobBuilder.newJob(SendEmail.class).withIdentity("SendEmail").build();
+        JobDetail job1 = JobBuilder.newJob(BlogPost.class).withIdentity("BlogPost").build();
 
         //trigger 생성
-        Trigger trigger = TriggerBuilder.newTrigger().
-                withSchedule(CronScheduleBuilder.cronSchedule("0 00 22 * * ?")).build();
+//        Trigger trigger = TriggerBuilder.newTrigger().
+//                withSchedule(CronScheduleBuilder.cronSchedule("0 00 22 * * ?")).build();
 
         Trigger trigger1 = TriggerBuilder.newTrigger().
-                withSchedule(CronScheduleBuilder.cronSchedule("0 0/5 * * * ?")).build();
+                withSchedule(CronScheduleBuilder.cronSchedule("0/10 * * * * ?")).build();
 
 //        startAt과 endAt을 사용해 job 스케쥴의 시작, 종료 시간도 지정할 수 있다.
 //        Trigger trigger = TriggerBuilder.newTrigger().startAt(startDateTime).endAt(EndDateTime)
