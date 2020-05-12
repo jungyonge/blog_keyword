6199f3640b2369d7cacd6b683dfaa04e588106af
Alphonse <jungyongchoi@coocha.com>
2020-05-12 15:18:14 +0900

    수정

--- src/main/java/blog/coupang/NamedGetAPI.java
+++ src/main/java/blog/coupang/NamedGetAPI.java
@@ -1,13 +1,23 @@
 package blog.coupang;
 
+import blog.model.BaseballModel;
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.util.EntityUtils;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public final class NamedGetAPI {
     private final static String REQUEST_METHOD = "GET";
@@ -37,7 +47,7 @@ public final class NamedGetAPI {
                 .addParameter("scores", "true")
                 .addParameter("specials", "true")
                 .addParameter("seasonTeamStat", "true")
-                .addParameter("startDate", "20200512")
+                .addParameter("startDate", "20200510")
                 .addParameter("endDate", "20200512")
                 .addParameter("v", String.valueOf(unixTime))
                 .build();
@@ -53,12 +63,28 @@ public final class NamedGetAPI {
             String json = EntityUtils.toString(httpResponse.getEntity());
 
             //JSON데이터를 넣어 JSON Object 로 만들어 준다.
-            JSONObject jsonObject = (JSONObject) jsonParser.parse(json);
+            JSONObject jsonObject = new JSONObject(json);
 
             //books의 배열을 추출
-            JSONArray bookInfoArray1 = (JSONArray) jsonObject.get("response");
+            JSONArray matchArr = jsonObject.getJSONArray("response");
 
-            System.out.println(bookInfoArray1);
+            for(int i = 0 ; i < matchArr.length() ; i++){
+                BaseballModel aTeamModel = new BaseballModel();
+                BaseballModel bTeamModel = new BaseballModel();
+
+                JSONObject matchObject = matchArr.getJSONObject(i);
+                aTeamModel.setGameId(matchObject.getJSONObject("odds").getString("gameId"));
+                bTeamModel.setGameId(matchObject.getJSONObject("odds").getString("gameId"));
+
+                aTeamModel.setLeague(matchObject.getJSONObject("league").getString("name"));
+                bTeamModel.setLeague(matchObject.getJSONObject("league").getString("name"));
+
+                aTeamModel.setGround(matchObject.getJSONObject("league").getString("name"));
+                bTeamModel.setGround(matchObject.getJSONObject("league").getString("name"));
+
+                JSONArray teamArr = matchArr.
+            }
+            System.out.println(matchArr);
         } catch (Exception e) {
             e.printStackTrace();
         }
